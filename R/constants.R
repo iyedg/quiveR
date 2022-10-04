c0ddbf47b691b03bff11231b090859c67bb448a9
Iyed Ghedamsi <iyed.ghedamsi@reach-initiative.org>
2022-10-04 10:07:13 +0100

    Added harmonized municipality and neighborhood names

---
+++ R/constants.R
@@ -0,0 +1,29 @@
+#' Harmonized names for Libyan Municipality names
+#'
+#' A mapping of Libyan Municipality names and a harmonized
+#' name used by REACH for that Municipality.
+#'
+#' @source \url{reach}
+#' @export
+harmonized_municipality_names <- c(
+  "Bint Bayya" = "Ubari",
+  "Gharb Azzawya" = "Azzawya",
+  "Wadi Etba" = "Murzuq",
+  "Swani Bin Adam" = "Al Aziziya",
+  "Alghrayfa" = "Ubari",
+  "Bint Bayya" = "Ubari",
+  "Alasabaa" = "Yefren",
+  "Edri" = "Brak"
+)
+
+#' Harmonized names for Libyan Neighborhood names
+#'
+#' A mapping of Libyan Neighborhood names and a harmonized
+#' name used by REACH for that Neighborhood.
+#'
+#' @source \url{reach}
+#' @export
+harmonized_neighborhood_names <- c(
+  "Ghadames" = "Nalut",
+  "Ghadames" = "Ghadamis"
+)
