5583824744dad178464d0240542872d4c0e081e8
Iyed Ghedamsi <iyed.ghedamsi@reach-initiative.org>
2022-10-06 08:38:20 +0100

    Added Libyan administrative levels datasets

--- R/data.R
+++ R/data.R
@@ -46,3 +46,46 @@
 #'
 #' @source \href{https://www.reachresourcecentre.info/}{REACH Resource Centre}
 "lby_msna2021_choices"
+
+#' Libyan Municipalities
+#'
+#' A dataset containing the identifiers as well as
+#' the Arabic and English names of Libyan Municipalities
+#'
+#' \describe{
+#'   \item{municipality_id}{A unique identifier for the Municipality}
+#'   \item{municipality_name_ar}{The Municipality's name in Arabic}
+#'   \item{municipality_name_en}{The Municipality's name in English}
+#'   \item{municipality_district_id}{The district to which the Municipality belongs}
+#'   \item{municipality_region_id}{The region to which the Municipality belongs}
+#' }
+#' @source \url{reach}
+"lby_municipalities"
+
+#' Libyan Districts
+#'
+#' A dataset containing the identifiers as well as
+#' the Arabic and English names of Libyan Districts
+#'
+#' \describe{
+#'   \item{district_id}{A unique identifier for the District}
+#'   \item{district_name_ar}{The Districts' name in Arabic}
+#'   \item{district_name_en}{The Districts' name in English}
+#'   \item{district_region_id}{The region to which the Districts belongs}
+#' }
+#' @source \url{reach}
+"lby_districts"
+
+
+#' Libyan Regions
+#'
+#' A dataset containing the identifiers as well as
+#' the Arabic and English names of Libyan Regions
+#'
+#' \describe{
+#'   \item{region_id}{A unique identifier for the Region}
+#'   \item{region_name_ar}{The Region's name in Arabic}
+#'   \item{region_name_en}{The Region's name in English}
+#' }
+#' @source \url{reach}
+"lby_regions"
